8e2ebbcc4ddf0567e03cf75bf4515cd68b48b425
gultaj <yaschick.mail@gmail.com>
2017-04-14 10:29:44 +0300

    add threads views, routes, tests

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/{thread}', 'ThreadsController@show')->name('threads.show');
